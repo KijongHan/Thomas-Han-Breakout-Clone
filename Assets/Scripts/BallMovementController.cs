39558403a7865171a16dc66b8968c771d6e73f31
Thomas Han <eyeseemint@gmail.com>
2021-11-16 18:52:42 +1300

    Add initial commit
    - Add urp and input system
    - Add initial scripts to test basic camera bounds functionality
    - https://answers.unity.com/questions/501893/calculating-2d-camera-bounds.html for understanding how to center camera for 2d views
    - https://forum.unity.com/threads/collision-with-sides-of-screen.228865/ for understanding how to add colliders to edges of camera

---
+++ Assets/Scripts/BallMovementController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovementController : MonoBehaviour
+{
+    public float BounceVelocity = 2F;
+
+    private new Rigidbody rigidbody;
+    private Vector3 velocity = new Vector3(0F, 0.5F, 0F);
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.velocity = velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGER ENTER");
+        rigidbody.velocity *= -1;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLISION ENTER");
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float MovementScale = 1.2F;
+
+    private new Rigidbody rigidbody;
+    private Vector3 movement;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        movement = new Vector3();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Camera.main.projectionMatrix);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.MovePosition(transform.TransformPoint(movement * Time.deltaTime * MovementScale));
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("FIRE");
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("MOVE");
+            movement = new Vector3(context.ReadValue<Vector2>().x, 0, 0);
+            //movement *= 0.05F;
+        }
+        if (context.canceled)
+        {
+            Debug.Log("STOP MOVE");
+            movement = new Vector3();
+        }
+    }
+}
